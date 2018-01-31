e65d354beea47402023dc4d8bf19364a535dab92
martena <36009784+martena@users.noreply.github.com>
2018-01-31 11:15:33 -0800

    completed assigment 1

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,16 @@
 // Student name:
 // This is where your first assignment goes!
+/**
+* The HelloWorldApp class implements an application that simply
+* displays "Hello World!" to the standard output.
+*/
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("cats are obviously the best animal");
+        System.out.println("magicarp is clearly the best pokemon");
+        System.out.println(" 10+10= the roaring twenties");
+        System.out.println("The lizard people that control the goverment killed JFK");
+        System.out.println("Demark is a real country unlike Canada which is not a real place");
+        System.out.println("January 31st ");
+    }
+}
